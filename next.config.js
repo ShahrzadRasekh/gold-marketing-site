e01193e1e1bbbc834fbccc4afac1f2e566017c09
ShahrzadRasekh <shahrzad.rasekh.marketing@gmail.com>
2025-11-13 14:41:47 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // ✅ Required for Server Actions in this template
-    serverActions: true,
-  },
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
   },
